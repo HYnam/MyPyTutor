bcb3f3f3f162832ecae57b6cfac1857008459e67
HYnam <namhy2006@gmail.com>
2021-04-19 21:58:52 +0800

    Hello World
    print Hello World

---
+++ W2_Intro.py
@@ -0,0 +1,18 @@
+"""
+Introduction: Hello, World!
+This is a simple example aimed at demonstrating the MyPy2 system.
+
+For this problem you need to write a program which outputs "Hello, World!". You can achieve this using the built-in print function.
+
+This window contains this text and the text box below contains all output from running tests on your code.
+
+The other window is almost identical to the window you use in IDLE for entering your code. You do the same here - entering your solution to the problem.
+
+When you have entered an answer press the “Run Code” button to evaluate your code. This will cause MyPy2 to check your solution. Running your code also saves your attempt. Optionally you can save your attempt at any time by clicking “Save Attempt”. In case you want to start over simply click on “Reset Attempt” to change the input window to the default code.
+
+If you get it right, all of the tests in this window will go green, and you should get no red output in the bottom (Analysis) box. If anything is printed to standard out, your output will also be displayed in blue text under the corresponding test. If your output is incorrect then the tests will be red and will show you a description of what went wrong. If your error is because your output and the test output did not match, then you will get a line beginning with a “?” that will show you how the strings are different and where.
+
+Occasionally, we require you to complete problems in a particular way (for example, by using a for loop). This will be enforced through hints in the question description. Even if you have the right answer, you may need to change your code a little in order to show us that you understand the concept we’re examining.
+"""
+
+print("Hello, World!")
