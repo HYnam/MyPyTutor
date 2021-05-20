00ddcf888552c8aa5dbf45363a5438fdfb22d769
HYnam <namhy2006@gmail.com>
2021-05-20 21:24:21 +0800

    Create W4_fun_square.py

---
+++ W4_fun_square.py
@@ -0,0 +1,16 @@
+"""
+Writing a Function
+    Write a function square(n) that takes an integer as input, and returns the square of the integer.
+"""
+
+def square(n) :
+    """Return the square of n.
+
+    Parameters:
+        n (int): Value to square 
+    
+    Return:
+        int: The square of n
+    """
+    # add your code here
+    return n ** 2
