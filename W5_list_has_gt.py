7106f67cfcc0a8386038afa51d2b90a2f3ee2c42
HYnam <namhy2006@gmail.com>
2021-06-23 15:04:56 +0800

    Create W5_list_has_gt.py

---
+++ W5_list_has_gt.py
@@ -0,0 +1,11 @@
+"""
+        Processing Lists Using a For Loop
+Lists are iterable objects and so for loops are typically used to process them.
+
+Write a function has_gt that takes a list of numbers and a number and returns True if the first argument contains a number greater than the second argument and False otherwise.
+"""
+def has_gt(myList, value):
+    for i in myList:
+        if i > value:
+            return True
+    return False    
