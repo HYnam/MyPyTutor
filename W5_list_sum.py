8855b3e5c063ad4107dcb23a34c3dbb23239e13c
HYnam <namhy2006@gmail.com>
2021-06-23 15:05:54 +0800

    Create W5_list_sum.py

---
+++ W5_list_sum.py
@@ -0,0 +1,11 @@
+"""
+        Summing Elements of a List
+Python comes with a built-in function sum that adds together all the elements of a list of numbers. For this problem you will write your own version.
+
+Write a function definition of sum_elems using a for loop that takes a list of numbers, and returns the sum of all the elements of the list. Your solution must not use the built-in function sum.
+"""
+def sum_elems(lst):
+    accumulator = 0
+    for element in lst:
+        accumulator += element
+    return accumulator
