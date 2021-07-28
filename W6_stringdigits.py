dd3aacebc4a51282a982610ef9bcb3efe9fa950b
HYnam <namhy2006@gmail.com>
2021-07-28 11:02:31 +0800

    Update W6_stringdigits.py

---
+++ W6_stringdigits.py
@@ -0,0 +1,14 @@
+"""
+                    Finding Digits in Strings
+This task is to implement a function get_digits(str1) that takes a string as input, and returns the string of digits in str1 in the order they appear in str1.
+
+You should use a for loop to iterate over the string.
+
+You will find a built-in string method very useful. Remember that you can get a list of all string method with dir(str), and help on a specific method with, eg, help(str.split).
+"""
+def get_digits(str1):
+    c = ""
+    for i in str1:
+        if i.isdigit():
+            c += i
+    return c
